27c285911e93714b184dee29f4f5b37141af191b
Obolo emmanuel <e.obolo@alustudent.com>
2024-01-15 02:05:46 +0200

    return to ald changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,8 +20,7 @@ class FileStorage:
     new(self, obj): sets in __objects the obj with key <obj class name>.id
     save(self): serializes __objects to the JSON file (path: __file_path)
     reload(self): deserializes the JSON file to __objects (only if the
-    JSON file (__file_path) exists ; otherwise, do nothing. If the file
-    doesn’ texist, no exception should be raised)
+    JSON file (__file_path) exists ; otherwise, do nothing. If the file doesn’t exist, no exception should be raised)
     """
 
     def __init__(self):
