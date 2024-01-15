b26a1aee935c7584ef5a4b7c7b24233b1e7a6f1e
Obolo emmanuel <e.obolo@alustudent.com>
2024-01-15 03:36:32 +0200

    hot fix

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -21,7 +21,7 @@ class TestFileStorage(unittest.TestCase):
     def test_file_storage_private_class_attributes(self):
         self.assertEqual(FileStorage.__file_path, "file.json")
         self.assertEqual(FileStorage.__objects, {})
-      
+   
     def test_all_method(self):
         model2 = BaseModel()
         model2.save()
@@ -31,38 +31,38 @@ class TestFileStorage(unittest.TestCase):
         self.assertTrue(f"{model2_class_name}.{model2_id}" in storage.all())
 
     def test_reload_method(self):
-      model = BaseModel()
-      model_id = model.id
-      model_class_name = type(model).__name__
-      model_key = f"{model_class_name}.{model_id}"
-      model_dict = storage.all()[model_key]
-      self.assertFalse("__class__" in model_dict)
-      model.save()
-      storage.reload()
-      model_dict = storage.all()[model_key]
-      self.assertTrue("__class__" in model_dict)
+        model = BaseModel()
+        model_id = model.id
+        model_class_name = type(model).__name__
+        model_key = f"{model_class_name}.{model_id}"
+        model_dict = storage.all()[model_key]
+        self.assertFalse("__class__" in model_dict)
+        model.save()
+        storage.reload()
+        model_dict = storage.all()[model_key]
+        self.assertTrue("__class__" in model_dict)
 
     def test_new_method(self):
-      prev_dict_length = len(storage.all())
-      new_added_model = BaseModel()
-      curr_dict_length = len(storage.all())
-      self.assertNotEqual(prev_dict_length, curr_dict_length)
-      new_added_model.save()
-      storage.reload()
+        prev_dict_length = len(storage.all())
+        new_added_model = BaseModel()
+        curr_dict_length = len(storage.all())
+        self.assertNotEqual(prev_dict_length, curr_dict_length)
+        new_added_model.save()
+        storage.reload()
 
     def test_save_method(self):
-      last_model = BaseModel()
-      self.assertIsInstance(last_model.created_at, datetime)
-      self.assertIsInstance(last_model.updated_at, datetime)
-      self.assertTrue("__class__" not in storage.all()
-                      [f"{type(last_model).__name__}.{last_model.id}"])
-      last_model.save()
-      self.assertIsInstance(
-          storage.all()[f"{type(last_model).__name__}.{last_model.id}"]
-          ["updated_at"], str)
-      self.assertIsInstance(
-          storage.all()[f"{type(last_model).__name__}.{last_model.id}"]
-          ["updated_at"], str)
-      self.assertTrue("__class__" in storage.all()
-                      [f"{type(last_model).__name__}.{last_model.id}"])
-      storage.reload()
+        last_model = BaseModel()
+        self.assertIsInstance(last_model.created_at, datetime)
+        self.assertIsInstance(last_model.updated_at, datetime)
+        self.assertTrue("__class__" not in storage.all()
+                        [f"{type(last_model).__name__}.{last_model.id}"])
+        last_model.save()
+        self.assertIsInstance(
+            storage.all()[f"{type(last_model).__name__}.{last_model.id}"]
+            ["updated_at"], str)
+        self.assertIsInstance(
+            storage.all()[f"{type(last_model).__name__}.{last_model.id}"]
+            ["updated_at"], str)
+        self.assertTrue("__class__" in storage.all()
+                        [f"{type(last_model).__name__}.{last_model.id}"])
+        storage.reload()
